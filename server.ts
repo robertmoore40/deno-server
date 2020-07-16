0b50c33ea9efb45de0b209d9b73fceabad1c436c
robertmoore40 <robertcmoore92@gmail.com>
2020-07-16 09:29:57 -0400

    added router functions

--- server.ts
+++ server.ts
@@ -3,3 +3,11 @@ import { readJson } from "https://deno.land/std/fs/mod.ts";
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 const router = new Router();
+router
+  .get("/", context => {
+    context.response.body = "Hello world!";
+  })
+  .get("/data", async context => {
+    const data = await readJson("./data.json");
+    context.response.body = data;
+  })
